58f19c355114deee6292e91cd63b32489cade815
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-30 16:54:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,8 @@ index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
-    false
-  else board[index] == "X" && "O"
-    true
+    return false
+  else board[index] == "X" || "O"
+    return true
   end
  end
