ad6e17c9ff9fc9d3fa5c9c3216f5d883be30655d
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-30 17:01:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 def position_taken?(board, index)
-    if board[index] == " " || "" || nil
+    if board[index] == " " || board[index] == "" || board[index] == nil
       return false
     else
       return true
