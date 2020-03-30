80743c1a5c191f9f9ad4524dd64361f493c647a2
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-30 16:43:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+def position_taken?(board, index)
+  if board[index] == "X" || "O"
+    true
+  else board[index] == " " || "" || nil
+    false
+  end
+ end
